f3363aa95ea99cd3a1902368aba4a0b1badd7150
carmirasdeli <52792085+carmirasdeli@users.noreply.github.com>
2020-03-05 08:45:25 -0500

    Add files via upload

--- currentStatus.js
+++ currentStatus.js
@@ -1,25 +1,26 @@
 function status() {
   var today = new Date(),
       open = "We're currently open!",
-      closed = "We're currently closed.",
-      display = document.getElementById('currentStatus');
-
-  if (today.getDay() == 0 && today.getHours() >= 9 && today.getHours() < 16) {
+      closed = "Sorry, we're closed.",
+      display = document.getElementById('currentStatus'),
+      time = today.getHours();
+  if (today.getDay() == 0 && time >= 9 && time < 16) {
       display.innerHTML = open;
-  } else if (today.getDay() == 1 && today.getHours() >= 8 && today.getHours() < 18) {
+  } else if (today.getDay() == 1 && time >= 8 && time < 18) {
       display.innerHTML = open;
-  } else if (today.getDay() == 2 && today.getHours() >= 8 && today.getHours() < 18) {
+  } else if (today.getDay() == 2 && time >= 8 && time < 18) {
       display.innerHTML = open;
-  } else if (today.getDay() == 3 && today.getHours() >= 8 && today.getHours() < 18) {
+  } else if (today.getDay() == 3 && time >= 8 && time < 18) {
       display.innerHTML = open;
-  } else if (today.getDay() == 4 && today.getHours() >= 8 && today.getHours() < 20) {
+  } else if (today.getDay() == 4 && time >= 8 && time < 20) {
       display.innerHTML = open;
-  } else if (today.getDay() == 5 && today.getHours() >= 8 && today.getHours() < 20) {
+  } else if (today.getDay() == 5 && time >= 8 && time < 20) {
       display.innerHTML = open;
-  } else if (today.getDay() == 6 && today.getHours() >= 8 && today.getHours() < 20) {
+  } else if (today.getDay() == 6 && time >= 8 && time < 20) {
       display.innerHTML = open;
   }
   else {
       display.innerHTML = closed;
+      display.style["background-color"] = "#FF3838"
   }
 }
