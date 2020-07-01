b27c8b2a1ba554bb14e7808a1b5dc7cb47ae2181
carmirasdeli <52792085+carmirasdeli@users.noreply.github.com>
2020-07-01 12:49:54 -0400

    Add files via upload

--- currentStatus.js
+++ currentStatus.js
@@ -6,17 +6,17 @@ function status() {
       time = today.getHours();
   if (today.getDay() == 0 && time >= 9 && time < 15) {
       display.innerHTML = open;
-  } else if (today.getDay() == 1 && time >= 8 && time < 18) {
+  } else if (today.getDay() == 1 && time >= 8 && time < 19) {
       display.innerHTML = open;
-  } else if (today.getDay() == 2 && time >= 8 && time < 18) {
+  } else if (today.getDay() == 2 && time >= 8 && time < 19) {
       display.innerHTML = open;
-  } else if (today.getDay() == 3 && time >= 8 && time < 18) {
+  } else if (today.getDay() == 3 && time >= 8 && time < 19) {
       display.innerHTML = open;
-  } else if (today.getDay() == 4 && time >= 8 && time < 18) {
+  } else if (today.getDay() == 4 && time >= 8 && time < 19) {
       display.innerHTML = open;
-  } else if (today.getDay() == 5 && time >= 8 && time < 18) {
+  } else if (today.getDay() == 5 && time >= 8 && time < 19) {
       display.innerHTML = open;
-  } else if (today.getDay() == 6 && time >= 8 && time < 18) {
+  } else if (today.getDay() == 6 && time >= 8 && time < 19) {
       display.innerHTML = open;
   }
   else {
