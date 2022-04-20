7b6172b5532618e8a38602e048b4713c64668784
carmirasdeli <52792085+carmirasdeli@users.noreply.github.com>
2022-04-20 18:50:36 -0400

    Add files via upload

---
+++ currentStatus.js
@@ -0,0 +1,26 @@
+function status() {
+  var today = new Date().getDay(),
+      time = new Date().getHours(),
+      open = "Now open!",
+      closed = "Closed now.",
+      display = document.getElementById('currentStatus');
+    if (today == 0 && time >= 9 && time < 15) {
+      display.innerHTML = open;
+  } else if (today == 1) {
+      display.innerHTML = closed;
+  } else if (today == 2 && time >= 10 && time < 19) {
+      display.innerHTML = open;
+  } else if (today == 3 && time >= 10 && time < 19) {
+      display.innerHTML = open;
+  } else if (today == 4 && time >= 10 && time < 19) {
+      display.innerHTML = open;
+  } else if (today == 5 && time >= 10 && time < 19) {
+      display.innerHTML = open;
+  } else if (today == 6 && time >= 10 && time < 19) {
+      display.innerHTML = open;
+  } else if (today == 6 && time >= 10 && time < 19) {
+      display.innerHTML = open;
+  } else {
+      display.innerHTML = closed;
+  }
+}
