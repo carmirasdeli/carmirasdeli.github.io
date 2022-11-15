9e8ee5e402e48299c68b9a12b9cdaacf375dadeb
carmirasdeli <52792085+carmirasdeli@users.noreply.github.com>
2022-11-15 09:47:03 -0500

    Add files via upload

--- auth.js
+++ auth.js
@@ -1,12 +1,11 @@
 function auth() {
-  let pass = "pass";
+  let pass = ["admin","pass"];
   let id = prompt("User ID:");
-  let authenticate = prompt("Password");
-  if (authenticate == pass && id == "admin") {
+  let authenticate = prompt("Password:");
+  if (authenticate == pass[1] && id == pass[0]) {
     document.getElementById("internal").style.display = "block";
     document.getElementById("logoutbutton").style.display = "block";
     document.getElementById("loginbutton").style.display = "none";
-
   } else {
       alert("User ID for Password failed")
   }
